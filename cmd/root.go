b045147af04fe68f1384e5bfb7c8cb11e282a95b
heitorfreitasferreira <heitor.ff@hotmail.com>
2024-09-21 14:51:18 -0300

    trocando para usar cobra-cli para ter diversos comandos (rodar 1 ou todos)

---
+++ cmd/root.go
@@ -0,0 +1,43 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "job-shop-ga",
+	Short: "Programa para resolver el problema de Job Shop Scheduling con algoritmos genéticos",
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.PersistentFlags().Int64("seed", 42, "Seed para el generador de números aleatorios")
+
+	rootCmd.PersistentFlags().Float64("mut", 0.3, "Experiment mutation rate")
+	rootCmd.PersistentFlags().Float64("cross", 0.4, "Experiment crossover rate")
+	rootCmd.PersistentFlags().Int("pop", 60, "Experiment population size")
+	rootCmd.PersistentFlags().Int("gen", 1000, "Experiment max generations")
+}
--- main.go
+++ main.go
@@ -1,27 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"flag"
-	"fmt"
-	"job-shop-ga/ga"
-)
+import "job-shop-ga/cmd"
 
 func main() {
-	mutationRate := flag.Float64("mut", 0.3, "Experiment mutation rate")
-	crossoverRate := flag.Float64("cross", 0.4, "Experiment crossover rate")
-	populationSize := flag.Int("pop", 60, "Experiment population size")
-	maxGenerations := flag.Int("gen", 1000, "Experiment max generations")
-
-	// Nome do arquivo de entrada
-	filename := "./benchmark/instances/abz6"
-
-	// Ler instância do problema
-	instance, err := ga.GetInstanceFromFile(filename, *mutationRate, *crossoverRate, *populationSize, *maxGenerations)
-	if err != nil {
-		fmt.Println("Erro ao ler o arquivo:", err)
-		return
-	}
-	instance.GenerateInitialPopulation()
-	instance.Print()
-	instance.Run()
+	cmd.Execute()
 }
